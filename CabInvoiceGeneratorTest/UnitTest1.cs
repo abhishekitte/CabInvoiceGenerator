a7a2885d7d8ff98d5a03ab259a2fb4c7d8f689cb
ABHI <abhishekitte11@gmail.com>
2021-10-24 13:45:50 +0530

    [ABHISHEK] Added a code to calculate total fare for multiple rides

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -1,12 +1,12 @@
 using CabInvoiceGenerator;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MStestforCabInvoice
+namespace CabInvoiceGeneratorTest
 {
     [TestClass]
     public class UnitTest1
     {
-        //UC1 Given total distance and time should calculate and return total fare (NORMAL) of the journey 
+        //UC1 Given total distance and time should calculate and return total fare of the journey 
         [TestMethod]
         [TestCategory("Fare")]
         public void GivenDistanceAndTimeShouldReturnNormalFare()
@@ -16,8 +16,21 @@ namespace MStestforCabInvoice
             int time = 5;
             //Calculating Fair
             double fare = invoiceGenerator.CalculateFare(distance, time);
-            double expected = 25;
+            double expected = 35;
             Assert.AreEqual(expected, fare);
+        }
+
+        //Uc2 calculate total fare for multiple ride
+        //Given number of rides should return total invoice summery
+        [TestMethod]
+        [TestCategory("Fare")]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] ride = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummery actual = invoiceGenerator.CalculateFare(ride);
+            InvoiceSummery expected = new InvoiceSummery(2, 60.0);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
